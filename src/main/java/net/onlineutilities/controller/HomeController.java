7fc3623cdc80306bb10e79c03eed6cdd49473e9a
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-08-18 22:54:16 +0700

    Add template engine and implement sample hompage

--- src/main/java/net/onlineutilities/controller/HomeController.java
+++ src/main/java/net/onlineutilities/controller/HomeController.java
@@ -1,16 +1,14 @@
 package net.onlineutilities.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @ResponseBody
     @GetMapping("/")
     public String index(){
-        return "Welcome to Onlineutilities.net";
+        return "home";
     }
 
 }
