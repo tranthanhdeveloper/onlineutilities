c840f91acc4d0ecda250048a8d54c4e0f8e84eca
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-09-06 11:40:32 +0700

    create template for triple encryption

--- src/main/java/net/onlineutilities/controller/EncryptionController.java
+++ src/main/java/net/onlineutilities/controller/EncryptionController.java
@@ -1,23 +1,32 @@
 package net.onlineutilities.controller;
 
+import net.onlineutilities.services.encrypt.EncryptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import net.onlineutilities.services.encrypt.EncryptService;
+import java.io.IOException;
 
 @Controller
 public class EncryptionController {
 	@Autowired
 	EncryptService encryptService;
+
+
 	
+	@GetMapping("/tripleencrypt")
+	public String index() {
+		return "encrypt/tripleencrypt";
+	}
+
 	@PostMapping("/tripleencrypt")
-	public String DESedeDecrypt() {
-		return "tripleencrypt";
+	public String encrypt(@RequestParam("file") MultipartFile file, @RequestParam("data") String data, Model model) throws IOException {
+		model.addAttribute("encryptedData", encryptService.encrypt(data, file.getBytes()));
+		return "encrypt/tripleencrypt";
 	}
 
 }
---
+++ src/main/java/net/onlineutilities/services/encrypt/EncryptServiceImpl.java
@@ -0,0 +1,17 @@
+package net.onlineutilities.services.encrypt;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class EncryptServiceImpl implements EncryptService {
+    @Override
+    public String encrypt(String data, byte[] keyAsBytes) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String data, byte[] keyAsBytes) {
+        return null;
+    }
+}
