c90bf6ff704a1554a3acb058b9cfb0227bcb02f5
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-10-06 22:00:18 +0700

    Modify index.html, home.html add filechecksum feature

--- src/main/java/net/onlineutilities/controller/HashController.java
+++ src/main/java/net/onlineutilities/controller/HashController.java
@@ -1,21 +1,11 @@
 package net.onlineutilities.controller;
 
-import java.io.IOException;
-import java.security.DigestException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.zip.CRC32;
-import java.util.zip.Checksum;
-
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("message-digest-tools")
@@ -93,43 +83,5 @@ public class HashController extends AbstractController {
         model.addAttribute("output", output);
         return "hash-tools/"+hashFunction.toLowerCase();
     }
-    
-    @GetMapping("file-checksum-generator.html")
-    public String fileChecksum() {    	
-        return "hash-tools/filechecksum";
-    }
-    
-    @PostMapping("file-checksum-generator.html")
-	public String fileChecksum(@RequestParam("algorithm") String algorithm, @RequestParam("file") MultipartFile file, Model model) throws IOException {
-		Map<String, String> resultMap = new HashMap<String, String>();
-		resultMap.put("file name", file.getName());
-		resultMap.put("file size", String.valueOf(file.getBytes().length));
-		try {
-			switch (algorithm.toLowerCase()) {
-			case "crc-32":
-				Checksum checksum = new CRC32();
-				checksum.update(file.getBytes(), 0, file.getBytes().length);
-				resultMap.put("CRC-32", Long.toHexString(checksum.getValue()));
-				break;
-			case "sha-1":
-				resultMap.put("SHA-1", Integer.toHexString(DigestUtils.getSha1Digest().digest(file.getBytes(), 0, file.getBytes().length)));
-				break;
-			case "sha-256":
-				resultMap.put("SHA-256", Integer.toHexString(DigestUtils.getSha256Digest().digest(file.getBytes(), 0, file.getBytes().length)));
-				break;
-			default:
-				Checksum checksumcrc32 = new CRC32();
-				checksumcrc32.update(file.getBytes(), 0, file.getBytes().length);
-				resultMap.put("CRC-32", Long.toHexString(checksumcrc32.getValue()));
-				resultMap.put("SHA-1", Integer.toHexString(DigestUtils.getSha1Digest().digest(file.getBytes(), 0, file.getBytes().length)));
-				resultMap.put("SHA-256", Integer.toHexString(DigestUtils.getSha256Digest().digest(file.getBytes(), 0, file.getBytes().length)));
-				break;
-			}
-		} catch (Exception e) {
-			logger.error("Error occured during compute file checksum\n"+ e.getStackTrace());
-		}
-		
-		model.addAttribute("result", resultMap);
-		return "hash-tools/filechecksum";
-	}
+
 }
