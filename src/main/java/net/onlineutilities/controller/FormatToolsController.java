f66353a6a4d992bf87b4ab7949e897069921b1e8
Thanh Tran <tranthanhdeveloper@gmail.com>
2021-06-27 11:45:27 +0700

    support sql format

--- src/main/java/net/onlineutilities/controller/FormatToolsController.java
+++ src/main/java/net/onlineutilities/controller/FormatToolsController.java
@@ -1,9 +1,9 @@
 package net.onlineutilities.controller;
 
+import com.github.vertical_blank.sqlformatter.SqlFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class FormatToolsController extends BaseController {
@@ -21,5 +21,16 @@ public class FormatToolsController extends BaseController {
     @GetMapping("css-format-online.html")
     public String beautifyCss() {
         return "format/css-format-online.html";
+    }
+
+    @GetMapping(value = "sql-format-online.html")
+    public String beautifySql() {
+        return "format/sql-format-online.html";
+    }
+
+    @PostMapping(value = "sql-format-online.html")
+    @ResponseBody
+    public String beautifySql(@RequestParam("sqlStatement") String sqlStatement) {
+        return SqlFormatter.format(sqlStatement);
     }
 }
