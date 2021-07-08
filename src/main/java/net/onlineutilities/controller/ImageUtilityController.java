115f72d42769b99fe467ac89a8275db02b267e52
Thanh Tran <tranthanhdeveloper@gmail.com>
2021-07-08 21:30:51 +0700

    Adjust code to meet AWS infrastructure.

--- src/main/java/net/onlineutilities/controller/ImageUtilityController.java
+++ src/main/java/net/onlineutilities/controller/ImageUtilityController.java
@@ -1,22 +1,21 @@
 package net.onlineutilities.controller;
 
+import net.onlineutilities.api.RestEnvelopeResponse;
 import net.onlineutilities.entity.FileToken;
 import net.onlineutilities.services.FileDownloadService;
 import net.onlineutilities.utils.SvgUtil;
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class ImageUtilityController extends BaseController {
@@ -27,47 +26,52 @@ public class ImageUtilityController extends BaseController {
     @GetMapping("convert-svg-to-{target}")
     public String svgConvert(@PathVariable("target") String targetFmt, Model model) {
         List<String> supportedFmt = Arrays.asList("png", "jpeg", "tiff");
-        if (!supportedFmt.contains(targetFmt.toLowerCase())){
+        if (!supportedFmt.contains(targetFmt.toLowerCase())) {
             return "error/404";
         }
         model.addAttribute("targetFmt", StringUtils.upperCase(targetFmt));
         return "images/svg-convert.html";
     }
 
-    @PostMapping("convert-svg-to-{target}")
-    public String svgConvert(MultipartFile file, @PathVariable("target") String targetFmt, Model model) {
-        model.addAttribute("modelSource", "post");
-        if (file != null && !file.isEmpty()) {
+    @PostMapping(value = "api/convert-svg-to-{target}", produces = "application/json", consumes = "multipart/form-data")
+    @ResponseBody
+    public RestEnvelopeResponse svgConvert(MultipartFile[] files, @PathVariable("target") String targetFmt) {
+        Map<String, String> error = new HashMap<>();
+        String data = null;
+        if (files != null && files.length > 0) {
             try {
                 File convertedFile = null;
-                switch (StringUtils.lowerCase(targetFmt)){
-                    case "png":{
-                        convertedFile = SvgUtil.convertTo(file, "png");
+                switch (StringUtils.lowerCase(targetFmt)) {
+                    case "png": {
+                        convertedFile = SvgUtil.convertTo(files, "png");
                         break;
-                    } case "jpeg": {
-                        convertedFile = SvgUtil.convertTo(file, "jpeg");
+                    }
+                    case "jpeg": {
+                        convertedFile = SvgUtil.convertTo(files, "jpeg");
                         break;
-                    } case "tiff": {
-                        convertedFile = SvgUtil.convertTo(file, "tiff");
+                    }
+                    case "tiff": {
+                        convertedFile = SvgUtil.convertTo(files, "tiff");
                         break;
-                    } default:{
-                        return "error/404";
+                    }
+                    default: {
+                        break;
                     }
                 }
                 String downloadToken = fileDownloadService.createNew(FileToken.builder().withFilePath(convertedFile.getAbsolutePath()).withFileName(convertedFile.getName()).build());
-                String originalFileName = file.getOriginalFilename();
-                String newFileName = originalFileName.substring(0, originalFileName.lastIndexOf('.')).concat("."+targetFmt.toLowerCase());
-
-                model.addAttribute("downloadLink", buildDownloadLink(downloadToken, newFileName));
-            } catch (IOException e) {
-                model.addAttribute("error", "Unable to convert your file");
-            } catch (TranscoderException e) {
-                model.addAttribute("error", "Unable to convert your file!");
+                data = buildDownloadLink(downloadToken, "onlineutiltites-svg-convert.zip");
+            } catch (Exception e) {
+                error.put("error", "Unable to convert your files");
             }
         } else {
-            model.addAttribute("error", "No file was upload, we can not continue to process!");
+            error.put("error", "No files was upload, we can not continue to process!");
         }
-        model.addAttribute("targetFmt", StringUtils.upperCase(targetFmt));
-        return "images/svg-convert.html";
+        return RestEnvelopeResponse
+                .builder()
+                .errors(error)
+                .statusCode(HttpStatus.OK.value())
+                .statusMessage(error.size() == 0 ? "" : "We are facing error during process request")
+                .data(data)
+                .build();
     }
 }
--- src/main/java/net/onlineutilities/utils/SvgUtil.java
+++ src/main/java/net/onlineutilities/utils/SvgUtil.java
@@ -1,5 +1,7 @@
 package net.onlineutilities.utils;
 
+import lombok.extern.log4j.Log4j2;
+import net.onlineutilities.wrapper.EnhancedZipEntry;
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
@@ -7,47 +9,76 @@ import org.apache.batik.transcoder.image.ImageTranscoder;
 import org.apache.batik.transcoder.image.JPEGTranscoder;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.batik.transcoder.image.TIFFTranscoder;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+@Log4j2
 public class SvgUtil {
 
-    public static File convertTo(MultipartFile file, String type) throws IOException, TranscoderException {
-        File tempSvgFile = Files.createTempFile("svg2png-", ".svg").toFile();
-        OutputStream outputStream = new FileOutputStream(tempSvgFile);
-        IOUtils.copy(file.getInputStream(), outputStream);
-        outputStream.flush();
+    public static File convertTo(MultipartFile[] files, String type) throws Exception {
+        FileOutputStream fos = null;
+        ZipOutputStream zipOutput;
+        List<EnhancedZipEntry> transcodeEntries = Arrays.stream(files).map(file -> {
+            try {
+                String filename = new StringBuffer(file.getOriginalFilename().substring(0, file.getOriginalFilename().lastIndexOf(".")))
+                        .append(".")
+                        .append(type.toLowerCase()).toString();
 
+                ZipEntry zipEntry = new ZipEntry(filename);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                TranscoderOutput transcoderOutput = new TranscoderOutput(byteArrayOutputStream);
+                TranscoderInput transcoderInput = new TranscoderInput(file.getInputStream());
+                final ImageTranscoder imageTranscoder = getImageTranscoder(type);
+                imageTranscoder.transcode(transcoderInput, transcoderOutput);
 
-        String svgUriInput = tempSvgFile.toURI().toURL().toString();
-        TranscoderInput transcoderInput = new TranscoderInput(svgUriInput);
+                return new EnhancedZipEntry(zipEntry, byteArrayOutputStream.toByteArray());
+            } catch (IOException | TranscoderException e) {
+                log.error("Error during transform svg format to {} format, message: {}", type, e.getMessage());
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toList());
 
-        File tempPngFile = Files.createTempFile("svg2png-", "."+type).toFile();
-        OutputStream pngOstream = new FileOutputStream(tempPngFile);
+        try {
+            File tempZipFile = Files.createTempFile("gzip-", ".zip").toFile();
+            fos = new FileOutputStream(tempZipFile);
+            zipOutput = new ZipOutputStream(new BufferedOutputStream(fos));
+            for (EnhancedZipEntry file : transcodeEntries) {
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(file.getBytes());
+                zipOutput.putNextEntry(file.getZipEntry());
+                zipOutput.write(byteArrayInputStream.readAllBytes());
+                zipOutput.flush();
+                zipOutput.closeEntry();
+            }
+            zipOutput.flush();
+            zipOutput.close();
+            return tempZipFile;
+        } catch (IOException e) {
+            throw new Exception("Error during zipping file");
+        }finally {
+            try{
+                if(fos != null) fos.close();
+            } catch(Exception ignored){
+            }
+        }
+    }
 
-        TranscoderOutput transcoderOutput = new TranscoderOutput(pngOstream);
+    private static ImageTranscoder getImageTranscoder(String type) {
         ImageTranscoder imageTranscoder = null;
-        if (type.equalsIgnoreCase("png")){
+        if (type.equalsIgnoreCase("png")) {
             imageTranscoder = new PNGTranscoder();
-        }else if (type.equalsIgnoreCase("jpeg")){
+        } else if (type.equalsIgnoreCase("jpeg")) {
             imageTranscoder = new JPEGTranscoder();
-        }else {
+        } else {
             imageTranscoder = new TIFFTranscoder();
         }
-        imageTranscoder.transcode(transcoderInput, transcoderOutput);
-        pngOstream.flush();
-        pngOstream.close();
-
-        outputStream.close();
-
-        Files.deleteIfExists(tempSvgFile.toPath());
-        return tempPngFile;
+        return imageTranscoder;
     }
 }
